adeb9d1ce6913ebffcb8ffbd54cd7ca8d482713d
Patrick <nekelpatrick@gmail.com>
2020-09-14 16:30:15 -0300

    Estilizei a parte básica de CSS

--- torre_hanoi.js
+++ torre_hanoi.js
@@ -1,6 +1,6 @@
 const numeroDePeças = 5 //pode ser um input !
 
-
+//Função criarDiv cria a div maior, o retângulo que engloba o jogo.
 function criarDiv() {
   let divGlobal = document.createElement("div");
   divGlobal.id = "id-divGlobal";
@@ -9,6 +9,8 @@ function criarDiv() {
   criarPecas()  
 }
 criarDiv()
+
+//Função criarTorre cria três torres e atribui id para elas conforme o loop.
 
 function criarTorre() {
     for (let index = 1; index < 4; index++) {
@@ -22,7 +24,7 @@ function criarTorre() {
   }
   
 
-
+//funcão CriarPecas  cria peças  em loop baseada na variável numeroDePeças que pode ser um  input do usuário
 function criarPecas () {
     for (let index = 0; index < numeroDePeças; index++) {
         let novaPeca = document.createElement("div")
